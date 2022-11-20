7eb6f75166bee79142a2afb5c18b7f8565a466ba
luscas <lcky123hop@gmail.com>
2022-11-20 11:04:30 -0300

    LUC-43 creation of models and relationships between them

---
+++ src/main/java/com/teste/integracao/spring/model/Frete.java
@@ -0,0 +1,37 @@
+package com.teste.integracao.spring.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Frete {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "codigo_frete")
+    private Integer id;
+
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "codigo_cliente", referencedColumnName = "codigo_cliente")
+    private Cliente cliente;
+
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "codigo_cidade", referencedColumnName = "codigo_cidade")
+    private Cidade cidade;
+
+    @Size(max = 30)
+    private String descricao;
+
+    @Size(max = 4)
+    private float peso;
+
+    @Size(max = 4)
+    private float valcr;
+}
