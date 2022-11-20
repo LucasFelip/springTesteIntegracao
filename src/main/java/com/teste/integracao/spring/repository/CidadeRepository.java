ff5d884d59775fb6c80f3ec5089953397e4cc5dd
luscas <lcky123hop@gmail.com>
2022-11-20 11:30:46 -0300

    LUC-43 define finders in repository class

--- src/main/java/com/teste/integracao/spring/repository/CidadeRepository.java
+++ src/main/java/com/teste/integracao/spring/repository/CidadeRepository.java
@@ -4,4 +4,5 @@ import com.teste.integracao.spring.model.Cidade;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+    Cidade findByNomeContaining(String nome);
 }
--- src/main/java/com/teste/integracao/spring/repository/FreteRepository.java
+++ src/main/java/com/teste/integracao/spring/repository/FreteRepository.java
@@ -3,5 +3,13 @@ package com.teste.integracao.spring.repository;
 import com.teste.integracao.spring.model.Frete;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FreteRepository extends JpaRepository<Frete, Integer> {
+    @Override
+    List<Frete> findAll();
+
+    Frete findById(Iterable<Integer> codigo_frete);
+
+    //List<Frete> findByCidade();
 }
