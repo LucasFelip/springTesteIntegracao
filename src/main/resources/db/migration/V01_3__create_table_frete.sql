442589bc71a3a2a8041f6b5f82eee9dfd513cd53
luscas <lcky123hop@gmail.com>
2022-11-20 11:11:01 -0300

    LUC-43 creation tables in database with flyway

---
+++ src/main/resources/db/migration/V01_3__create_table_frete.sql
@@ -0,0 +1,13 @@
+CREATE TABLE frete (
+  codigo_frete INT NOT NULL,
+   codigo_cliente INT NULL,
+   codigo_cidade INT NULL,
+   descricao VARCHAR(30) NULL,
+   peso FLOAT NOT NULL,
+   valcr FLOAT NOT NULL,
+   CONSTRAINT pk_frete PRIMARY KEY (codigo_frete)
+);
+
+ALTER TABLE frete ADD CONSTRAINT FK_FRETE_ON_CODIGO_CIDADE FOREIGN KEY (codigo_cidade) REFERENCES cidade (codigo_cidade);
+
+ALTER TABLE frete ADD CONSTRAINT FK_FRETE_ON_CODIGO_CLIENTE FOREIGN KEY (codigo_cliente) REFERENCES cliente (codigo_cliente);
