e00ab829441406619862995bbdcad66239beadf8
luscas <lcky123hop@gmail.com>
2022-11-20 11:05:07 -0300

    LUC-43 creation of repository to models

---
+++ src/main/java/com/teste/integracao/spring/repository/ClienteRepository.java
@@ -0,0 +1,7 @@
+package com.teste.integracao.spring.repository;
+
+import com.teste.integracao.spring.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+}
