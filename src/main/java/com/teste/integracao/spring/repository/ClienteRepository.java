771b8e1f23e69fb31a36e1dd58cb5570e4672afe
luscas <lcky123hop@gmail.com>
2022-11-20 11:30:46 -0300

    LUC-43 define finders in repository class

--- src/main/java/com/teste/integracao/spring/repository/ClienteRepository.java
+++ src/main/java/com/teste/integracao/spring/repository/ClienteRepository.java
@@ -4,4 +4,5 @@ import com.teste.integracao.spring.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+    Cliente findByNomeContaining(String nome);
 }
