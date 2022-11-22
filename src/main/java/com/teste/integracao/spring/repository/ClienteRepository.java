7fdad6266b7b57e8248f4f6309e4a6accf08ec41
Lucas Reis <lcky123hop@gmail.com>
2022-11-22 18:02:04 -0300

    LUC-43 Update repository project insert @repository

--- src/main/java/com/teste/integracao/spring/repository/ClienteRepository.java
+++ src/main/java/com/teste/integracao/spring/repository/ClienteRepository.java
@@ -2,11 +2,13 @@ package com.teste.integracao.spring.repository;
 
 import com.teste.integracao.spring.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-    Cliente findByNomeContaining(String nome);
+    List<Cliente> findByNomeContaining(String nome);
 
     Cliente findByFretes_Id(Integer id);
-
-
 }
