7630f1bc4669ec70a997d1d0f0e0da99ded017ab
Lucas Reis <lcky123hop@gmail.com>
2022-11-23 20:23:00 -0300

    LUC-43 Create test for CidadeRepositoryTest.java

---
+++ src/test/java/com/teste/integracao/spring/respository/CidadeRepositoryTest.java
@@ -0,0 +1,20 @@
+package com.teste.integracao.spring.respository;
+
+import com.teste.integracao.spring.repository.CidadeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.jdbc.Sql;
+
+public class CidadeRepositoryTest {
+    private CidadeRepository repository;
+
+    @BeforeEach
+    void init() {
+
+    }
+
+    @Test
+    public void insertTest() {
+
+    }
+}
