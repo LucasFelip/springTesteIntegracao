edf4b826f1231f83485a0b76f7f9c5869751e0a9
Lucas Reis <lcky123hop@gmail.com>
2022-11-25 17:19:14 -0300

    LUC-43 Create controller to Cliente and Frete with ResponseEntity

---
+++ src/main/java/com/teste/integracao/spring/controller/ClienteController.java
@@ -0,0 +1,106 @@
+package com.teste.integracao.spring.controller;
+
+import com.teste.integracao.spring.domain.model.Cliente;
+import com.teste.integracao.spring.domain.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+    @Autowired
+    private final ClienteService service;
+
+    public ClienteController(ClienteService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<Cliente> listarTodos(){
+        var optional = service.todos();
+
+        if (!optional.isEmpty()){
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscaPor(@PathVariable Integer id) {
+        var optional = service.buscaPor(id);
+
+        if (optional.isPresent()) {
+            return ResponseEntity.ok(optional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{nome}")
+    public ResponseEntity<Cliente> buscaPor(@PathVariable String nome) {
+        var optional = service.buscaPor(nome);
+
+        if (!optional.isEmpty()) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/frete/{id}")
+    public ResponseEntity<Cliente> buscarPorFretes_Id(@PathVariable Integer id) {
+        var optional = service.buscarPorFretes_Id(id);
+
+        if (optional != null) {
+            return ResponseEntity.ok().build();
+        } else {
+          return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> cadastro(@RequestBody @Valid Cliente cliente,
+                                            UriComponentsBuilder builder) {
+
+        final Cliente clienteSalvo = service.salva(cliente);
+        final URI uri = builder
+                .path("/clientes/{id}")
+                .buildAndExpand(clienteSalvo.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(clienteSalvo );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualiza(@PathVariable Integer id,
+                                            @RequestBody @Valid Cliente cliente) {
+        Optional<Cliente> optional = service.buscaPor(id);
+
+        if (optional.isPresent()) {
+            cliente.setId(id);
+            Cliente clienteAtualizado = service.salva(cliente);
+            return ResponseEntity.ok(clienteAtualizado);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> remover(@PathVariable Integer id) {
+        Optional<Cliente> optional = service.buscaPor(id );
+
+        if (optional.isPresent()) {
+            service.removePelo(id);
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+}
---
+++ src/main/java/com/teste/integracao/spring/controller/FreteController.java
@@ -0,0 +1,112 @@
+package com.teste.integracao.spring.controller;
+
+import com.teste.integracao.spring.domain.model.Frete;
+import com.teste.integracao.spring.domain.service.FreteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/fretes")
+public class FreteController {
+    @Autowired
+    private final FreteService service;
+
+    public FreteController(FreteService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<Frete> listarTodos(){
+        var optional = service.todos();
+
+        if (!optional.isEmpty()){
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Frete> buscaPor(@PathVariable Integer id) {
+        var optional = service.buscaPor(id);
+
+        if (optional.isPresent()) {
+            return ResponseEntity.ok(optional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/cliente/{id}")
+    public ResponseEntity<Frete> buscaPorCliente_Id(@PathVariable Integer id) {
+        var optional = service.buscaPorCliente_Id(id);
+
+        if (optional != null) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/cidade/{id}")
+    public ResponseEntity<Frete> buscaPorCidade_Id(@PathVariable Integer id) {
+        var optional = service.buscaPorCidade_Id(id);
+
+        if (optional != null) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/cliente/order/{id}")
+    public ResponseEntity<Frete> buscarPorCliente_IdOrderByValcrAsc(@PathVariable Integer id) {
+        var optional = service.buscarPorCliente_IdOrderByValcrAsc(id);
+
+        if (optional != null) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Frete> cadastro(@RequestBody @Valid Frete frete, UriComponentsBuilder builder) {
+        final Frete freteSalvo = service.salva(frete);
+        final URI uri = builder
+                .path("/clientes/{id}")
+                .buildAndExpand(freteSalvo.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(freteSalvo);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Frete> atualiza(@PathVariable Integer id,
+                                          @RequestBody @Valid Frete frete) {
+        var optional = service.buscaPor(id);
+
+        if(optional.isPresent()){
+            frete.setId(id);
+            Frete freteAtualizado = service.salva(frete);
+            return ResponseEntity.ok(freteAtualizado);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> remover(@PathVariable Integer id) {
+        var optional = service.buscaPor(id);
+
+        if (optional.isPresent()) {
+            service.removePelo(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
