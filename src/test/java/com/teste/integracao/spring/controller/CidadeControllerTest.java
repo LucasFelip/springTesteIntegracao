b91bb73dc9db79abb4e2a64fbb65c06f645c4e96
Lucas Reis <lcky123hop@gmail.com>
2022-11-28 21:43:15 -0300

    LUC-43 Resolve bug not auth (but created another bug kk)

--- src/test/java/com/teste/integracao/spring/controller/CidadeControllerTest.java
+++ src/test/java/com/teste/integracao/spring/controller/CidadeControllerTest.java
@@ -1,37 +1,48 @@
 package com.teste.integracao.spring.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.teste.integracao.spring.domain.repository.CidadeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.*;
 
 import com.teste.integracao.spring.domain.model.Cidade;
+import org.springframework.test.web.servlet.MockMvc;
 
-@Transactional
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@WebMvcTest(CidadeController.class)
 public class CidadeControllerTest {
+    @MockBean
+    private CidadeRepository repository;
 
     @Autowired
-	private TestRestTemplate testRestTemplate;
+	private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
-    public void deveSalvarCidade(){
+    void deveSalvarCidade() throws Exception {
+        Cidade cidade = Cidade.builder().nome("Null City").taxa(10).build();
 
-        Cidade cidade = Cidade.builder().nome("Null City").build();
+        mockMvc.perform(post("/cidades").contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(cidade)))
+                .andExpect(status().isCreated())
+                .andDo(print());
+/*
         HttpEntity<Cidade> httpEntity = new HttpEntity<Cidade>(cidade);
 
         ResponseEntity<Cidade> resposta = testRestTemplate.exchange(
             "/cidades", HttpMethod.POST, httpEntity, Cidade.class
         );
 
-        assertEquals(HttpStatus.CREATED, resposta.getStatusCode());
+        assertEquals(HttpStatus.CREATED, resposta.getStatusCode());*/
     }
 }
