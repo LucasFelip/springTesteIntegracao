7bcca1f783b4702c90ee77ac53fec4ffde217760
Lucas Reis <lcky123hop@gmail.com>
2022-11-30 20:46:17 -0300

    LUC-43 Remodel migration

--- src/main/resources/db/migration/V01_2__create_table_cidade.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE cidade (
-  codigo_cidade INT NOT NULL AUTO_INCREMENT,
-   nome VARCHAR(30) NULL,
-   uf VARCHAR(2) NULL,
-   taxa FLOAT NOT NULL,
-   CONSTRAINT pk_cidade PRIMARY KEY (codigo_cidade)
-);
