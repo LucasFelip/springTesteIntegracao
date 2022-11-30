503426acdd2fe9ef0ecc10a74a07fd8731b28f8e
Lucas Reis <lcky123hop@gmail.com>
2022-11-30 15:52:45 -0300

    LUC-43 CidadeControllerTest.java not finalized

--- src/test/java/com/teste/integracao/spring/controller/CidadeControllerTest.java
+++ src/test/java/com/teste/integracao/spring/controller/CidadeControllerTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@Transactional
 @SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CidadeControllerTest {
     @Autowired
