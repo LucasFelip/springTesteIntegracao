55f1363781451ee350dd2805b2190555b2e46605
Lucas Felipe <75092912+LucasFelip@users.noreply.github.com>
2023-01-06 16:46:56 -0300

    Update CI/CD (#18)
    * LUC-43 remove tests broken
    
    * LUC-43 update tests.yml
    
    * LUC-43 update tests.yml
    
    * LUC-43 update tests.yml
    
    * LUC-43 delete dotnet.yml
    
    Co-authored-by: Lucas Reis <lcky123hop@gmail.com>

--- src/main/java/com/teste/integracao/spring/Application.java
+++ src/main/java/com/teste/integracao/spring/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class }) // annotation to exclude error security
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
